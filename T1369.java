edbc3d7d7aa0c19721921ef6650743f0b0a103d8
younghust <532486390@qq.com>
2015-09-16 10:49:55 +0800

    优化注释

--- T1369.java
+++ T1369.java
@@ -23,23 +23,23 @@ public class T1369 {
 
 	public static void Permuration(char[] arrays, int k, int length) {
 
-		System.out.println("进入一层递归，当前指针在 " + k);
-		System.out.println("指针在 " + k + " 的位置上，对第 " + k + " 个字符之后的字符串全排列");
+//		System.out.println("进入一层递归，当前指针在 " + k);
+//		System.out.println("指针在 " + k + " 的位置上，对第 " + k + " 个字符之后的字符串全排列");
 
 		int j;
 		if (k == length) {
 			for (j = 0; j < length; j++) {
 				System.out.print(arrays[j]);
 			}
-			System.out.println("---------------");
+			System.out.println("");
 		} else {
 			for (j = k; j < length; j++) {
 				if (k != j && arrays[j] == arrays[k]) {
 					continue;
 				}
 				swap(arrays, k, j);
-				System.out.println("交换第 " + k + " 个字符与第 " + j
-						+ "个字符，然后再对其后所有字符全排列");
+//				System.out.println("交换第 " + k + " 个字符与第 " + j
+//						+ "个字符，然后再对其后所有字符全排列");
 				// printArray(arrays);System.out.println();
 				bubbleSort(arrays, k + 1, length);
 				// printArray(arrays);System.out.println();
@@ -47,7 +47,7 @@ public class T1369 {
 				bubbleSort(arrays, k + 1, length);
 			}
 		}
-		System.out.println("跳出一层递归，当前指针回到 " + k + " 位置");
+//		System.out.println("跳出一层递归，当前指针回到 " + k + " 位置");
 	}
 
 	public static void bubbleSort(char[] arrays, int begin, int length) {
